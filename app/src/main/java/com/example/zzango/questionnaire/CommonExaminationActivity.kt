b9f633f7ee5e7e952a0173010f88f003715e6945
zzango <sch1774@nate.com>
2018-12-21 12:14:46 +0900

    공통문진표

--- app/src/main/java/com/example/zzango/questionnaire/CommonExaminationActivity.kt
+++ app/src/main/java/com/example/zzango/questionnaire/CommonExaminationActivity.kt
@@ -2,6 +2,9 @@ package com.example.zzango.questionnaire
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_common_exam.*
+
 
 class CommonExaminationActivity : AppCompatActivity() {
 
@@ -10,57 +13,57 @@ class CommonExaminationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_common_exam)
 
-//        diagnosis_medication_etc_examination_check.setOnCheckedChangeListener {
-//
-//            buttonView, isChecked ->
-//
-//            if(isChecked){
-//
-//                diagnosis_medication_etc_disease_name.visibility = View.VISIBLE
-//                diagnosis_medication_etc_disease_name_input.visibility = View.VISIBLE
-//
-//            }else{
-//
-//                if(!diagnosis_medication_etc_medication_check.isChecked){
-//
-//                    diagnosis_medication_etc_disease_name.visibility = View.GONE
-//                    diagnosis_medication_etc_disease_name_input.visibility = View.GONE
-//
-//                }
-//
-//            }
-//
-//        }
-//
-//        diagnosis_medication_etc_medication_check.setOnCheckedChangeListener {
-//
-//            buttonView, isChecked ->
-//
-//            if(isChecked){
-//
-//                diagnosis_medication_etc_disease_name.visibility = View.VISIBLE
-//                diagnosis_medication_etc_disease_name_input.visibility = View.VISIBLE
-//
-//            }else{
-//
-//                if(!diagnosis_medication_etc_examination_check.isChecked){
-//
-//                    diagnosis_medication_etc_disease_name.visibility = View.GONE
-//                    diagnosis_medication_etc_disease_name_input.visibility = View.GONE
-//
-//                }
-//
-//            }
-//
-//        }
-//
-//        diagnosis_medication_etc_disease_name_input.setOnFocusChangeListener {
-//
-//            v, hasFocus ->
-//
-//            v.clearFocus()
-//
-//        }
+        diagnosis_medication_etc_examination_check.setOnCheckedChangeListener {
+
+            buttonView, isChecked ->
+
+            if(isChecked){
+
+                diagnosis_medication_etc_disease_name.visibility = View.VISIBLE
+                diagnosis_medication_etc_disease_name_input.visibility = View.VISIBLE
+
+            }else{
+
+                if(!diagnosis_medication_etc_medication_check.isChecked){
+
+                    diagnosis_medication_etc_disease_name.visibility = View.GONE
+                    diagnosis_medication_etc_disease_name_input.visibility = View.GONE
+
+                }
+
+            }
+
+        }
+
+        diagnosis_medication_etc_medication_check.setOnCheckedChangeListener {
+
+            buttonView, isChecked ->
+
+            if(isChecked){
+
+                diagnosis_medication_etc_disease_name.visibility = View.VISIBLE
+                diagnosis_medication_etc_disease_name_input.visibility = View.VISIBLE
+
+            }else{
+
+                if(!diagnosis_medication_etc_examination_check.isChecked){
+
+                    diagnosis_medication_etc_disease_name.visibility = View.GONE
+                    diagnosis_medication_etc_disease_name_input.visibility = View.GONE
+
+                }
+
+            }
+
+        }
+
+        diagnosis_medication_etc_disease_name_input.setOnFocusChangeListener {
+
+            v, hasFocus ->
+
+            v.clearFocus()
+
+        }
 
     }
 }
