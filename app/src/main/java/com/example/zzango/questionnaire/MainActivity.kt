39765dabff0be1b56cae84f9e42d86f657a63e80
min92822 <jeong920822@gmail.com>
2018-12-26 11:30:16 +0900

    메인 와이파이 연결여부 ing

--- app/src/main/java/com/example/zzango/questionnaire/MainActivity.kt
+++ app/src/main/java/com/example/zzango/questionnaire/MainActivity.kt
@@ -1,21 +1,53 @@
 package com.example.zzango.questionnaire
 
 
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
 import com.example.zzango.questionnaire.LocalList.ListActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.save_location.view.*
 
 class MainActivity : AppCompatActivity() , View.OnClickListener {
+
+    var popup = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var wfm = getApplicationContext().getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+        getSharedPreferences("connection_state", Context.MODE_PRIVATE)
+
+        if(wfm.isWifiEnabled){
+
+            getSharedPreferences("connection_state", Context.MODE_PRIVATE).edit().putString("wifi", "").apply()
+
+            data_save_mode.setImageResource(R.drawable.ic_rss_feed)
+
+            println("와이파이")
+
+        }else{
+
+            getSharedPreferences("connection_state", Context.MODE_PRIVATE).edit().putString("local", "").apply()
+
+            data_save_mode.setImageResource(R.drawable.ic_sd_storage)
+
+            println("와이파이 ㄴ")
+
+        }
         supportFragmentManager.beginTransaction()
                 .add(R.id.fragment_right, FirstFragment()).commit()
 
@@ -32,6 +64,87 @@ class MainActivity : AppCompatActivity() , View.OnClickListener {
         button3.setOnClickListener(this)
         button4.setOnClickListener(this)
         button5.setOnClickListener(this)
+
+    }
+    fun saveAlert(){
+
+        var dialog = AlertDialog.Builder(this).create()
+        var dialog_view = LayoutInflater.from(this).inflate(R.layout.save_location, null)
+
+        dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        dialog.setView(dialog_view)
+        dialog_view.save_location_text.setText("저장할 곳을 골라주세요")
+
+        if(!popup) {
+
+            dialog.show().let {
+
+                popup = true
+
+            }
+
+        }
+
+        var displayMetrics = DisplayMetrics()
+        dialog.window.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        // The absolute width of the available display size in pixels.
+        var displayWidth = displayMetrics.widthPixels
+        // The absolute height of the available display size in pixels.
+        var displayHeight = displayMetrics.heightPixels
+
+        // Initialize a new window manager layout parameters
+        var layoutParams = WindowManager.LayoutParams()
+
+        // Copy the alert dialog window attributes to new layout parameter instance
+        layoutParams.copyFrom(dialog.window.attributes)
+
+        // Set the alert dialog window width and height
+        // Set alert dialog width equal to screen width 90%
+        // int dialogWindowWidth = (int) (displayWidth * 0.9f);
+        // Set alert dialog height equal to screen height 90%
+        // int dialogWindowHeight = (int) (displayHeight * 0.9f);
+
+        // Set alert dialog width equal to screen width 70%
+        var dialogWindowWidth = (displayWidth * 0.7f).toInt()
+        // Set alert dialog height equal to screen height 70%
+        var dialogWindowHeight = ViewGroup.LayoutParams.WRAP_CONTENT
+
+        // Set the width and height for the layout parameters
+        // This will bet the width and height of alert dialog
+        layoutParams.width = dialogWindowWidth
+        layoutParams.height = dialogWindowHeight
+
+        // Apply the newly created layout parameters to the alert dialog window
+        dialog.window.attributes = layoutParams
+
+
+        dialog.setOnDismissListener {
+
+            popup = false
+            dialog = null
+
+        }
+
+        dialog_view.local.setOnClickListener {
+
+            data_save_mode.setImageResource(R.drawable.ic_sd_storage)
+
+            getSharedPreferences("connection_state", Context.MODE_PRIVATE).edit().putString("local", "").apply()
+
+            dialog.dismiss()
+
+        }
+
+        dialog_view.server.setOnClickListener {
+
+            data_save_mode.setImageResource(R.drawable.ic_rss_feed)
+
+            getSharedPreferences("connection_state", Context.MODE_PRIVATE).edit().putString("wifi", "").apply()
+
+            dialog.dismiss()
+
+        }
 
     }
 
