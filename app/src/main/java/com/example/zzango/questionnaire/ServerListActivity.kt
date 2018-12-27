990ae0503a324847b0ce2d9cebc3d0ee2547ff1e
zzango <sch1774@nate.com>
2018-12-27 15:08:40 +0900

    서버 리스트

---
+++ app/src/main/java/com/example/zzango/questionnaire/ServerListActivity.kt
@@ -0,0 +1,44 @@
+package com.example.zzango.questionnaire
+
+import android.app.Activity
+import android.os.Bundle
+
+class ServerListActivity : Activity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.server_list_activity)
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
