1ac8b252e964b316445f7eebf11993d66a3a75f7
min92822 <jeong920822@gmail.com>
2018-12-27 14:20:50 +0900

    검진 클릭시 로딩 추가 (공통문진만)

--- app/src/main/java/com/example/zzango/questionnaire/FirstFragment.kt
+++ app/src/main/java/com/example/zzango/questionnaire/FirstFragment.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_first.view.*
 
 class FirstFragment : Fragment(){
@@ -16,13 +17,25 @@ class FirstFragment : Fragment(){
 
         view.first_imageButton1.setOnClickListener {
 
+            activity!!.login_appbar_loading_progress.visibility = View.VISIBLE
+            activity!!.login_appbar_loading_progress_bg.visibility = View.VISIBLE
+
             startActivity(Intent(activity!!, CommonExaminationActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP))
 
         }
 
 
         return view
+
     }
 
+    override fun onResume() {
+
+        activity!!.login_appbar_loading_progress.visibility = View.GONE
+        activity!!.login_appbar_loading_progress_bg.visibility = View.GONE
+
+        super.onResume()
+
+    }
 
 }
