114ab3d0cfb8c388be216a7ca0c1ab28a21f60e3
zzango <sch1774@nate.com>
2019-01-02 14:39:20 +0900

    추가 문진표

---
+++ app/src/main/java/com/example/zzango/questionnaire/MentalExaminationActivity.kt
@@ -0,0 +1,18 @@
+package com.example.zzango.questionnaire
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class MentalExaminationActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mental_exam)
+
+
+
+    }
+
+}
