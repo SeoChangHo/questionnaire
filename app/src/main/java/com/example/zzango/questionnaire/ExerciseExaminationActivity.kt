71376c7dec6e606995d40970da06c32b513908dc
min92822 <jeong920822@gmail.com>
2019-01-07 09:26:51 +0900

    액티비티 세로고정 생활습관 평가도구 운동 xml fin

---
+++ app/src/main/java/com/example/zzango/questionnaire/ExerciseExaminationActivity.kt
@@ -0,0 +1,15 @@
+package com.example.zzango.questionnaire
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ExerciseExaminationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise_exam)
+
+    }
+
+}
