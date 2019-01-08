9f4f0b6361f238dea2d3b9bc52e7e74a563860c6
min92822 <jeong920822@gmail.com>
2019-01-08 11:50:16 +0900

    영양 액티비티 ing

---
+++ app/src/main/java/com/example/zzango/questionnaire/NutritionExaminationActivity.kt
@@ -0,0 +1,17 @@
+package com.example.zzango.questionnaire
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class NutritionExaminationActivity :AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nutrition_exam)
+
+
+
+    }
+
+}
