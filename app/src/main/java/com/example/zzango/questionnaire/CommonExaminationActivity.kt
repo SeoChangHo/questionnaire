295c9c9c5c87b4013fbdf227d74edea231ec2b92
min92822 <jeong920822@gmail.com>
2019-01-23 10:36:03 +0900

    코드 클린업 & 저장 버튼 텍스트 조건에 맞춰 변경

--- app/src/main/java/com/example/zzango/questionnaire/CommonExaminationActivity.kt
+++ app/src/main/java/com/example/zzango/questionnaire/CommonExaminationActivity.kt
@@ -351,9 +351,9 @@ class CommonExaminationActivity : RootActivity() {
 
             GetPaper(paper)
         }else{
-            name_edit.setText(MainActivity.login_user_name)
-            first_serial.setText(MainActivity.user_first_serial)
-            last_serial.setText(MainActivity.user_last_serial)
+            name_edit.text = MainActivity.login_user_name
+            first_serial.text = MainActivity.user_first_serial
+            last_serial.text = MainActivity.user_last_serial
 
             if(MainActivity.chart != "SET1"){
 
@@ -492,7 +492,7 @@ class CommonExaminationActivity : RootActivity() {
         dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
         dialog.setView(dialog_view)
-        dialog_view.save_complete_alert_text.setText("저장이 완료 되었습니다")
+        dialog_view.save_complete_alert_text.text = "저장이 완료 되었습니다"
 
         if (!popup) {
 
@@ -1179,9 +1179,9 @@ class CommonExaminationActivity : RootActivity() {
     fun GetPaper(paper: Paper_COMMON) {
 
 
-        name_edit.setText(paper.name)
-        first_serial.setText(paper.first_serial)
-        last_serial.setText(paper.last_serial)
+        name_edit.text = paper.name
+        first_serial.text = paper.first_serial
+        last_serial.text = paper.last_serial
 
         println(paper)
 
