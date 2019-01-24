33a2d05afa9470b9770c5f1f7a3a3dc4a6905f37
min92822 <jeong920822@gmail.com>
2019-01-24 16:41:41 +0900

    userlist 추가

--- app/src/main/java/com/example/zzango/questionnaire/UserList/UserList.kt
+++ app/src/main/java/com/example/zzango/questionnaire/UserList/UserList.kt
@@ -11,8 +11,11 @@ var USER1PASS = "1q2w3e4r"
 var USER2 = "changho"
 var USER2PASS = "changho"
 
+var USER3 = "qq"
+var USER3PASS ="1111"
+
 
 object User
 {
-    val Map: HashMap<String, String> = hashMapOf(USER1 to USER1PASS, USER2 to USER2PASS)
+    val Map: HashMap<String, String> = hashMapOf(USER1 to USER1PASS, USER2 to USER2PASS, USER3 to USER3PASS)
 }
