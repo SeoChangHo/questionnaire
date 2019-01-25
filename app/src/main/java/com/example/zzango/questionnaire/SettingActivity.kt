6d9a0f39d9eafeba4d7a8918a719552fcb06c348
zzango <sch1774@nate.com>
2019-01-25 11:08:20 +0900

    설정

--- app/src/main/java/com/example/zzango/questionnaire/SettingActivity.kt
+++ app/src/main/java/com/example/zzango/questionnaire/SettingActivity.kt
@@ -2,8 +2,13 @@ package com.example.zzango.questionnaire
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.widget.Button
 import com.example.zzango.questionnaire.LocalList.ListActivity
 import kotlinx.android.synthetic.main.activity_setting.*
 
@@ -14,6 +19,7 @@ class SettingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
 
+        manager_logout.setText(MainActivity.manager_name)
         var num = getSharedPreferences("connection", Context.MODE_PRIVATE).getString("state", "")
 
 
@@ -31,6 +37,44 @@ class SettingActivity : AppCompatActivity() {
             }else{
                 getSharedPreferences("connection", Context.MODE_PRIVATE).edit().putString("state", "local").apply()
             }
+        }
+
+        manager_logout.setOnClickListener {
+
+            var dialog = AlertDialog.Builder(this).create()
+            var dialog_view = LayoutInflater.from(this).inflate(R.layout.activity_user_logout, null)
+
+            //다이얼로그 뒤로가기 버튼 막기
+            dialog.setCancelable(false)
+
+            dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+            dialog.setView(dialog_view)
+            dialog.setCanceledOnTouchOutside(false)
+
+            val logout = dialog_view.findViewById(R.id.user_logout) as Button
+            val cancel = dialog_view.findViewById(R.id.user_logout_cancel) as Button
+
+            dialog.show()
+
+            logout.setOnClickListener {
+
+                MainActivity.login_user_name = ""
+                MainActivity.user_first_serial = ""
+                MainActivity.user_last_serial = ""
+
+                //view.text = "사용자 등록하기"
+                //view2.setImageResource(R.drawable.regi)
+
+                startActivity(Intent(this@SettingActivity, MainActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP))
+
+                dialog.dismiss()
+            }
+
+            cancel.setOnClickListener {
+                dialog.dismiss()
+            }
+
         }
 
 
