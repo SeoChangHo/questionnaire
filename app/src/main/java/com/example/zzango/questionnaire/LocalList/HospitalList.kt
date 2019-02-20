ca28c200f497ad832617a6d037c81f9e5a394eb5
zzango <sch1774@nate.com>
2019-02-20 14:30:45 +0900

    병원별 로고변경

---
+++ app/src/main/java/com/example/zzango/questionnaire/LocalList/HospitalList.kt
@@ -0,0 +1,20 @@
+package com.example.zzango.questionnaire.LocalList
+
+class HospitalList {
+
+
+    object hospital
+    {
+
+        //테스트병원
+        val test = "test"
+
+        //목포한국병원
+        val Mokpo = "Mokpo"
+
+        //반포
+        val Banpo = "Banpo"
+
+    }
+
+}
