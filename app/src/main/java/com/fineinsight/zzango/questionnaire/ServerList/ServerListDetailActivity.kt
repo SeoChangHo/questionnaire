81d42fdb458a8e10b129495dbef6a1df88b5c071
jeongyeon <traeumen927@naver.com>
2019-07-30 09:57:03 +0900

    ddd

--- app/src/main/java/com/fineinsight/zzango/questionnaire/ServerList/ServerListDetailActivity.kt
+++ app/src/main/java/com/fineinsight/zzango/questionnaire/ServerList/ServerListDetailActivity.kt
@@ -2,8 +2,10 @@ package com.fineinsight.zzango.questionnaire.ServerList
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import com.fineinsight.zzango.questionnaire.DataClass.SelectDetailInfo
 import com.fineinsight.zzango.questionnaire.R
+import kotlinx.android.synthetic.main.activity_server_list_detail.*
 
 class ServerListDetailActivity : AppCompatActivity() {
 
@@ -22,14 +24,13 @@ class ServerListDetailActivity : AppCompatActivity() {
         {
             SelectDetailArr = intent.getSerializableExtra("ARR") as ArrayList<SelectDetailInfo>
 
-
-
             for (item in SelectDetailArr)
             {
                 println("TABLENAME: ${item.TableName}")
             }
+
+            serverdetail_recyclertView.layoutManager = LinearLayoutManager(this@ServerListDetailActivity)
+            serverdetail_recyclertView.adapter = ServerListDetailAdapter(SelectDetailArr, this@ServerListDetailActivity)
         }
-//                        server_recyclertView.layoutManager = LinearLayoutManager(this@ServerListActivity)
-////                        server_recyclertView.adapter = ServerListAdapter(userList, this@ServerListActivity)
     }
 }
---
+++ app/src/main/java/com/fineinsight/zzango/questionnaire/ServerList/ServerListDetailAdapter.kt
@@ -0,0 +1,53 @@
+package com.fineinsight.zzango.questionnaire.ServerList
+
+
+import android.support.constraint.ConstraintLayout
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.fineinsight.zzango.questionnaire.DataClass.SelectDetailInfo
+import com.fineinsight.zzango.questionnaire.R
+
+
+class ServerListDetailAdapter (var userList: ArrayList<SelectDetailInfo>,val Activity : ServerListDetailActivity) : RecyclerView.Adapter<ServerListDetailAdapter.ViewHolder>(){
+
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        val view: View = LayoutInflater.from(p0.context).inflate(R.layout.serverdetail_list_layout, p0, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+
+        return userList.size
+
+    }
+
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+
+        p0.userName.text = userList[p1].userName
+        p0.userTable.text = userList[p1].TableName
+        //p0.userGender.text = userList[p1].userNumber
+
+
+        p0.listlayoutBack.setOnClickListener {
+
+        }
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    {
+        //라벨
+        val userName: TextView = itemView.findViewById(R.id.txtName)
+        val userTable: TextView = itemView.findViewById(R.id.txtTable)
+        val userGender: TextView = itemView.findViewById(R.id.txtGender)
+
+
+        val listlayoutBack: ConstraintLayout = itemView.findViewById(R.id.server_list_layout_back)
+    }
+
+
+}
