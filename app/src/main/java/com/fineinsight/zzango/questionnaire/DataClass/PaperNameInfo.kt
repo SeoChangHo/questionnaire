5bf88cc305bed169e50ec709fbc746c304eb590c
jeongyeon <traeumen927@naver.com>
2019-08-13 11:45:07 +0900

    gd

---
+++ app/src/main/java/com/fineinsight/zzango/questionnaire/DataClass/PaperNameInfo.kt
@@ -0,0 +1,26 @@
+package com.fineinsight.zzango.questionnaire.DataClass
+
+class PaperNameInfo {
+
+    //PaperCategoryClass
+    data class PC_Class(
+            val KO_NM:String,
+            var EN_NM:String
+    )
+
+
+    //PaperCategory
+    object PC
+    {
+        val ORAL = PC_Class("구강", "ORAL")
+        val COMMON = PC_Class("공통", "COMMON")
+        val MENTAL = PC_Class("정신건강", "MENTAL")
+        val COGNITIVE = PC_Class("인지기능", "COGNITIVE")
+        val ELDERLY = PC_Class("노인기능", "ELDERLY")
+        val EXERCISE = PC_Class("운동", "EXERCISE")
+        val NUTRITION = PC_Class("영양", "NUTRITION")
+        val SMOKING = PC_Class("흡연", "SMOKING")
+        val DRINKING = PC_Class("음주", "DRINKING")
+        val CANCER = PC_Class("암", "CANCER")
+    }
+}
