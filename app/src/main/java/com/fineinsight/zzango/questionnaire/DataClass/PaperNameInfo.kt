456c45967df9ed76b80c97ba931c152b4e956a0b
jeongyeon <traeumen927@naver.com>
2019-08-13 11:49:46 +0900

    gd

--- app/src/main/java/com/fineinsight/zzango/questionnaire/DataClass/PaperNameInfo.kt
+++ app/src/main/java/com/fineinsight/zzango/questionnaire/DataClass/PaperNameInfo.kt
@@ -17,10 +17,15 @@ class PaperNameInfo {
         val MENTAL = PC_Class("정신건강", "MENTAL")
         val COGNITIVE = PC_Class("인지기능", "COGNITIVE")
         val ELDERLY = PC_Class("노인기능", "ELDERLY")
+        val LIFE = PC_Class("생활습관", "LIFE")
+        val CANCER = PC_Class("암", "CANCER")
+
+        //생활습관 4개항목
+        /*
         val EXERCISE = PC_Class("운동", "EXERCISE")
         val NUTRITION = PC_Class("영양", "NUTRITION")
         val SMOKING = PC_Class("흡연", "SMOKING")
         val DRINKING = PC_Class("음주", "DRINKING")
-        val CANCER = PC_Class("암", "CANCER")
+         */
     }
 }
