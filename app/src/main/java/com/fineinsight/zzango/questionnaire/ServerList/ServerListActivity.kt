d5fc2202c1275a895820b1487039a9e138d8b387
jeongyeon <traeumen927@naver.com>
2019-08-19 12:12:54 +0900

    ㅎㅇㄹ

--- app/src/main/java/com/fineinsight/zzango/questionnaire/ServerList/ServerListActivity.kt
+++ app/src/main/java/com/fineinsight/zzango/questionnaire/ServerList/ServerListActivity.kt
@@ -1,6 +1,8 @@
 package com.fineinsight.zzango.questionnaire.ServerList
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
@@ -16,6 +18,9 @@ import kotlinx.android.synthetic.main.server_list_activity.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.time.LocalDate
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ServerListActivity : Activity() {
 
@@ -24,16 +29,64 @@ class ServerListActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.server_list_activity)
 
+        DateSetting()
         loadList()
+    }
 
+    fun DateSetting()
+    {
+        txtDate.text = LocalDate.now().toString()
+
+
+        txtDate.setOnClickListener {
+
+            var MYyear = 0
+            var MYmonth = 0
+            var MYday = 0
+
+
+            var splitdate = txtDate.text.toString().split("-")
+            var CheckMYyear = splitdate[0].toIntOrNull()
+            var CheckMYmonth = splitdate[1].toIntOrNull()
+            var CheckMYday = splitdate[2].toIntOrNull()
+
+            println("splitdate: ${splitdate}")
+            println("CheckMYyear: ${CheckMYyear}")
+            println("CheckMYmonth: ${CheckMYmonth}")
+            println("CheckMYday: ${CheckMYday}")
+
+            if (CheckMYyear != null && CheckMYmonth != null && CheckMYday != null)
+            {
+                MYyear = splitdate[0].toInt()
+                MYmonth = splitdate[1].toInt()-1
+                MYday = splitdate[2].toInt()
+
+                val dp = DatePickerDialog(this, AlertDialog.THEME_HOLO_DARK, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                    txtDate.text = "${year}-${(month+1).toString().padStart(2, '0')}-${dayOfMonth.toString().padStart(2, '0')}"
+                    loadList()
+                }, MYyear, MYmonth, MYday)
+
+                dp.setOnCancelListener {
+
+                }
+
+                dp.datePicker.maxDate = Calendar.getInstance().timeInMillis
+
+                dp.show()
+            }
+        }
     }
 
 
     fun loadList(){
 
+        var EMPTYList = ArrayList<SelectInfo>()
+        server_recyclertView.layoutManager = LinearLayoutManager(this@ServerListActivity)
+        server_recyclertView.adapter = ServerListAdapter(EMPTYList, this@ServerListActivity)
+
+
         var selectDate = java.util.HashMap<String, String>()
-        //selectDate["DATE"] = LocalDate.now().toString()
-        selectDate["DATE"] = "2019-07-05"
+        selectDate["DATE"] = txtDate.text.toString()
         selectDate["AREA"] = MainActivity.hospital
 
         ProgressAction(true)
