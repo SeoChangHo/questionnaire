0a241771ee7e0cff1845bf5a7e23152052934366
jeongyeon <traeumen927@naver.com>
2019-09-17 14:11:04 +0900

    ddd

--- app/src/main/java/com/fineinsight/zzango/questionnaire/ServerList/ServerListAgreeAdapter.kt
+++ app/src/main/java/com/fineinsight/zzango/questionnaire/ServerList/ServerListAgreeAdapter.kt
@@ -26,7 +26,11 @@ class ServerListAgreeAdapter (var userList: ArrayList<SelectInfo>, val Activity 
     override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
 
         val JUMIN = userList[p1].userNumber
-        val LASTNUM = JUMIN.substring(JUMIN.length-1, JUMIN.length).toIntOrNull()
+
+        val FirstJUMIN = JUMIN.substring(0, 6)
+        val LastJUMIN = JUMIN.substring(6)
+
+        val LASTNUM = JUMIN.substring(LastJUMIN.length-1, LastJUMIN.length).toIntOrNull()
 
         if (LASTNUM != null)
         {
@@ -34,7 +38,7 @@ class ServerListAgreeAdapter (var userList: ArrayList<SelectInfo>, val Activity 
         }
 
         p0.userName.text = userList[p1].userName
-        p0.userJumin.text = JUMIN.substring(0, JUMIN.length-1)
+        p0.userJumin.text = FirstJUMIN
         p0.listlayoutBack.setOnClickListener {
 
             Activity.loadDetailList(
